bc17096ce4ffb1251c34d0c7f67b067fe9efab89
NFLEagles500 <NFLEagles500@gmail.com>
2023-12-10 10:02:06 -0700

    Update main.py
    added some exception handling for when it connects to the internet to get current date and time after initial boot

--- main.py
+++ main.py
@@ -6,16 +6,48 @@ References:
     https://www.engineersgarage.com/micropython-esp32-modem-light-deep-sleep-modes-timer-external-touch-wake-up/#:~:text=There%20are%20two%20external%20%E2%80%9Cwake,of%20its%20capacitive%20touch%20pins.
     https://docs.micropython.org/en/latest/esp32/quickref.html
 '''
-import time
+import utime
 import uos
 import machine
 from neopixel import NeoPixel
 import esp32
 
+#lower CPU freq
+print(f'CPU is running at: {machine.freq()}')
+#machine.freq(60000000)
+
+def utcToLocal(type):
+    #get the offset from timeapi.io, using your timezone
+    global localUtcOffset
+    localTime = utime.localtime(utime.time() + localUtcOffset)
+    if type == 'time':
+        return f'{localTime[3]:02d}:{localTime[4]:02d}:{localTime[5]:02d}'
+    elif type == 'date':
+        return f'{localTime[0]}/{localTime[1]}/{localTime[2]:02d}'
+    else:
+        return f'{localTime[0]}/{localTime[1]}/{localTime[2]:02d} {localTime[3]:02d}:{localTime[4]:02d}:{localTime[5]:02d}'
+
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(envSecrets.ssid, envSecrets.wifipsw)
+    iter = 1
+    while wlan.ifconfig()[0] == '0.0.0.0':
+        print(f'Not Connected...{iter}')
+        iter += 1
+        utime.sleep(1)
+        if iter == 10:
+            wlan.disconnect()
+            wlan.connect(envSecrets.ssid, envSecrets.wifipsw)
+            iter = 1
+    ip = wlan.ifconfig()[0]
+    print(f'{network.hostname()} is connected on {ip}')
+
 def tone(pin,frequency,duration):
     pin.freq(frequency)
     pin.duty_u16(30000)
-    time.sleep_ms(duration)
+    utime.sleep_ms(duration)
     pin.duty_u16(0)
 
 resetCauseNames = ['PWRON_RESET','HARD_RESET','WDT_RESET','DEEPSLEEP_RESET','SOFT_RESET','BROWN_OUT_RESET']
@@ -37,11 +69,9 @@ blink_iter = 1
 while blink_iter > 0:
     np.fill((0, 0, 150))  # Set the NeoPixel blue
     np.write()  # Write data to the NeoPixel
-    time.sleep(0.5)  # Pause for 0.5 seconds
-#    np.fill((0, 0, 0))  # Turn the NeoPixel off
-#    np.write()  # Write data to the NeoPixel
-    time.sleep(0.5)  # Pause for 0.5 seconds
+    utime.sleep(0.5)  # Pause for 0.5 seconds
     blink_iter = blink_iter - 1
+utime.sleep(5)
 #deepsleep will NOT process interrupts, it will only restart the microcontroller
 #interrupt_pin.irq(trigger=Pin.IRQ_RISING, handler=wake_up)
 
@@ -55,24 +85,54 @@ oneVolt = 1162
 #actual_voltage = (readVoltage / oneVolt) * (resistor_1 + resistor_2) / resistor_2
 
 batt_adc_power.value(1)
-time.sleep(1)
+utime.sleep(1)
 actual_voltage = (battery_adc.read() / oneVolt) * (resistor_1 + resistor_2) / resistor_2
 if resetCause == 'DEEPSLEEP_RESET':
     wakeReason = next(item for item in wakeReasons if machine.wake_reason() == getattr(machine, item))
+    currDateTime = f'{utime.localtime()[0]}/{utime.localtime()[1]}/{utime.localtime()[2]} {utime.localtime()[3] - 7:02d}:{utime.localtime()[4]:02d}:{utime.localtime()[5]:02d}'
     with open('log.txt','a') as file:
-        file.write(f'Reset Cause: {resetCause} Wake Reason: {wakeReason} Battery voltage: {actual_voltage:.2f}\n')
-    print(f'Reset Cause: {resetCause} Wake Reason: {wakeReason} Battery voltage: {actual_voltage:.2f}')
+        file.write(f'{currDateTime} Reset Cause: {resetCause} Wake Reason: {wakeReason} Battery voltage: {actual_voltage:.2f}\n')
+    
+    print(f'{currDateTime} Reset Cause: {resetCause} Wake Reason: {wakeReason} Battery voltage: {actual_voltage:.2f}')
 else:
+    import network
+    import envSecrets
+    import ntptime
+    import urequests
+    wlan = network.WLAN(network.STA_IF)
+    connect()
+    utime.sleep(1)
+    while True:
+        try:
+            ntptime.settime()
+            break
+        except Exception as e:
+            print(f'trying ntptime again... Exception: {e}')
+            utime.sleep(1)
+    while True:
+        try:
+            responseFromTimeapi = urequests.get(envSecrets.timeApiUrl)
+            break
+        except Exception as e:
+            print(f'Error from urequests, {e}, trying again...')
+            utime.sleep(1)
+    localUtcOffset = responseFromTimeapi.json()['currentUtcOffset']['seconds']
+    responseFromTimeapi.close()
     with open('log.txt','a') as file:
-        file.write(f'Reset Cause: {resetCause} Battery voltage: {actual_voltage:.2f}\n')
-    print(f'Reset Cause: {resetCause} Battery voltage: {actual_voltage:.2f}')
+        file.write(f'{utcToLocal('datetime')} Reset Cause: {resetCause} Battery voltage: {actual_voltage:.2f}\n')
+    print(f'{utcToLocal('datetime')} Reset Cause: {resetCause} Battery voltage: {actual_voltage:.2f}')
 batt_adc_power.value(0)
 if actual_voltage < 4.40:
     buzzer = machine.PWM(machine.Pin(15),duty_u16=0)
     tone(buzzer,2500,1000)
+currDateTime = f'{utime.localtime()[0]}/{utime.localtime()[1]}/{utime.localtime()[2]} {utime.localtime()[3] - 7:02d}:{utime.localtime()[4]:02d}:{utime.localtime()[5]:02d}'
+with open('log.txt','a') as file:
+        file.write(f'{currDateTime} Reset Cause: {resetCause}, going to deepsleep\n')
     
 '''
 #Use esp32.wake_on_ext0 to allow a GPIO pin change wake it
 '''
 esp32.wake_on_ext0(interrupt_pin,esp32.WAKEUP_ANY_HIGH)
+power_pin.off()
+#machine.deepsleep(10000)
 machine.deepsleep(900000000)
